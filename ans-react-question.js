6ff892c5b5661520adfba3b8f1a67550ebc79e25
Wendy Chen <lfwendy@gmail.com>
2024-10-14 20:41:30 +0800

    Update ans-react-question.js

--- ans-react-question.js
+++ ans-react-question.js
@@ -1,4 +1,4 @@
-3.
+// 3.
 /**
 let items = [1, 1, 1, 5, 2, 3, 4, 3, 3, 3, 3, 3, 3, 7, 8, 5, 4, 9, 0, 1, 3, 2, 6, 7, 5, 4, 4, 7, 8, 8, 0, 1, 2, 3, 1];
 Please write down a function to console log unique value from this array.
